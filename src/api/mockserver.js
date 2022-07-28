8607f6e66b81dba1c340adba3eced0964f1e5504
dodotdot <rm.herwidodo@gmail.com>
2022-07-28 21:44:22 +0700

    update axios and mockserver

--- src/api/mockserver.js
+++ src/api/mockserver.js
@@ -1,27 +1,44 @@
-// src/server.js
-import { createServer, Model } from "miragejs"
+import { Server, Model } from 'miragejs'
 
-export function makeServer({ environment = "development" } = {}) {
-  let server = createServer({
+export function makeServer ({ environment = 'development' } = {}) {
+  let server = new Server({
     environment,
-
     models: {
-      user: Model,
+      todos: Model
     },
-
-    seeds(server) {
-      server.create("user", { name: "Bob" })
-      server.create("user", { name: "Alice" })
+    seeds (server) {
+      server.create('todo', { todo_item: 'Get Up' })
+      server.create('todo', { todo_item: 'Take A Shower' })
+      server.create('todo', { todo_item: 'Go To Work' })
     },
+    routes () {
+      this.namespace = 'api'
 
-    routes() {
-      this.namespace = "api"
-
-      this.get("/users", (schema) => {
-        return schema.users.all()
+      this.get('/todos', schema => {
+        return schema.todos.all()
       })
-    },
-  })
 
+      this.get('/todos/:id', (schema, request) => {
+        let id = request.params.id
+        return schema.todos.find(id)
+      })
+
+      this.post('/todos', (schema, request) => {
+        let todo_item = JSON.parse(request.requestBody).todo_item
+        return schema.todos.create({ todo_item })
+      })
+
+      this.del('/todos/:id', (schema, request) => {
+        let id = request.params.id
+        schema.todos.find(id).destroy()
+      })
+
+      this.patch('/todos/:id', (schema, request) => {
+        let id = request.params.id
+        let newTodo = JSON.parse(request.requestBody).todo_item
+        schema.todos.find(id).update({ todo_item: newTodo })
+      })
+    }
+  })
   return server
 }
