c0b2a05005017cdf20549331775d95e52bcba452
dodotdot <rm.herwidodo@gmail.com>
2022-10-11 20:45:56 +0700

    update birth date

--- src/pages/students/AddStudent.vue
+++ src/pages/students/AddStudent.vue
@@ -33,7 +33,7 @@ const schema = Yup.object().shape({
         .required('Email/username harus diisi'),
     birthPlace: Yup.string()
         .required('Tempat Lahir harus diisi'),
-    birthDate: Yup.date()
+    userBirthDate: Yup.date()
         .transform(function (value, originalValue) {
         if (this.isType(value)) {
             return value;
@@ -101,9 +101,9 @@ async function onSubmit(values) {
           </div>
 
           <div class="relative mb-6">
-              <Field name="birthDate" type="text" id="birthDate" class="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-200 bg-transparent rounded-full border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-200 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-gray-300 peer" placeholder=" " />
-              <label for="birthDate" class="absolute text-sm text-gray-200 dark:text-gray-400 duration-300 transform -translate-y-0 scale-75 top-2 z-10 origin-[0] bg-transparent dark:bg-gray-20 px-2 peer-focus:px-2 peer-focus:text-gray-400 peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-0 left-1">Tanggal Lahir Santri (ex: 31-12-2000)</label>
-              <label class=" text-red-500 text-sm p-2">{{ errors.birthDate }}</label>
+              <Field name="userBirthDate" type="text" id="userBirthDate" class="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-200 bg-transparent rounded-full border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-200 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-gray-300 peer" placeholder=" " />
+              <label for="userBirthDate" class="absolute text-sm text-gray-200 dark:text-gray-400 duration-300 transform -translate-y-0 scale-75 top-2 z-10 origin-[0] bg-transparent dark:bg-gray-20 px-2 peer-focus:px-2 peer-focus:text-gray-400 peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-0 left-1">Tanggal Lahir Santri (ex: 31-12-2000)</label>
+              <label class=" text-red-500 text-sm p-2">{{ errors.userBirthDate }}</label>
           </div>
 
 
